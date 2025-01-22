c9f039c772cae341f4db2e5eec676514230af23b
Fucci <benjaminakacha@gmail.com>
2025-01-21 16:26:56 -0800

    update: about card sophistication

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -5,42 +5,35 @@ import { ImPointRight } from "react-icons/im";
 function AboutCard() {
   return (
     <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hello, I am <span className="purple">Amine Akacha (Fucci Online) </span>
-            from <span className="purple"> Toulouse, France.</span>
-            <br />
-            I am currently working as a Freelance Software Developer
-            <br />
-            I have completed 3 years of coursework in advanced mathematics, including topics such as calculus, linear algebra, and statistical analysis, at Universite Paul-Sabatier III.
-            Specialized in discrete mathematics, gaining foundational knowledge in algorithms, graph theory, and combinatorics.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Reading (Favorite books range from Dostoevsky to Cervantes and then back to Toni Morrison)
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Fixing things (I get unlogically excited at the though of having a Green light to take something apart in order to fix it)
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling (I have lived across 3 continents and feel an enthralling urge to discover both city and nature.)
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> LEarning (I have an unquenchable thirst for learning about EVERYTHING.)
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Nothing is as permanent as a temporary solution that works"{" "}
-          </p>
-          <footer className="blockquote-footer">Reddit probably</footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
+    <Card.Body>
+      <blockquote className="blockquote mb-0">
+        <p style={{ textAlign: "justify" }}>
+          Hi, I’m <span className="purple">Amine Akacha</span>, a Freelance Software Developer based in the <span className="purple">USA.</span>.
+          With a strong foundation in advanced mathematics and discrete structures, I specialize in building efficient and scalable solutions.
+          <br />
+          <br />
+          Outside of work, I enjoy:
+        </p>
+        <ul>
+          <li className="about-activity">
+            <ImPointRight /> Reading classic and contemporary literature
+          </li>
+          <li className="about-activity">
+            <ImPointRight /> Solving technical challenges and fixing things
+          </li>
+          <li className="about-activity">
+            <ImPointRight /> Exploring new cultures through travel
+          </li>
+        </ul>
+  
+        <p style={{ color: "rgb(155 126 172)", marginTop: "20px" }}>
+          "Simplicity is the ultimate sophistication."
+        </p>
+        <footer className="blockquote-footer">Leonardo da Vinci</footer>
+      </blockquote>
+    </Card.Body>
+  </Card>
+  
   );
 }
 
